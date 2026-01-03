4049ff4b64fef16e1adf150e6cd2bfd716981ffd
dev_wandry <dima.development@wandry.com.ua>
2026-01-03 14:00:06 +0200

    feat(seo): added open graph

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,31 @@ export const metadata: Metadata = {
   title: "Scout",
   description:
     "Keep your components from multiple registries up to date — safely and automatically.",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://scoutcn.dev",
+    title: "Scout",
+    description:
+      "Keep your components from multiple registries up to date — safely and automatically.",
+    siteName: "Scout",
+    images: [
+      {
+        url: `https://scoutcn.dev/opengraph-image.png`,
+        width: 1200,
+        height: 630,
+        alt: "Scout",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Scout",
+    description:
+      "Keep your components from multiple registries up to date — safely and automatically.",
+    images: [`https://scoutcn.dev/opengraph-image.png`],
+    creator: "@kapish_dima",
+  },
 };
 
 export default function RootLayout({
