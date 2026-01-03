3dbedc810e76dfad8b7417f8a3fd8c06958abffa
dev_wandry <dima.development@wandry.com.ua>
2026-01-03 02:32:59 +0200

    feat(analytics): Added Vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
-import { Oswald, IBM_Plex_Mono } from "next/font/google";
+
+import { Analytics } from "@vercel/analytics/next";
+
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -16,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body>{children}</body>
+      <Analytics />
     </html>
   );
 }
